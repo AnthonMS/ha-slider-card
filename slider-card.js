be76629e03586006152ba90e14e93cabe096ed63
AnthonMS <Anthon@Steiness.info>
2021-04-24 21:26:49 +0200

    Fan is now supported and vertical slider is now possible, but very finicky.

--- slider-card.js
+++ slider-card.js
@@ -15,7 +15,7 @@ static get properties() {
 
 constructor() {
   super();
-  console.log("SLIDER-CARD V10 INSTALLED--------------------------------------------------------------");
+  // console.log("SLIDER-CARD V36 INSTALLED--------------------------------------------------------------");
 }
 
 render() {
@@ -23,27 +23,35 @@ render() {
   var width = this.config.width ? this.config.width : "100%";
   var height = this.config.height ? this.config.height : "50px";
   var radius = this.config.radius ? this.config.radius : "4px";
+  var top = this.config.top ? this.config.top : "0px";
+  var bottom = this.config.bottom ? this.config.bottom : "0px";
+  var right = this.config.right ? this.config.right : "0px";
+  var left = this.config.left ? this.config.left : "0px";
+  var rotate = this.config.rotate ? this.config.rotate : "0";
+  var containerHeight = this.config.containerHeight ? this.config.containerHeight : height;
+  if (rotate != "0") {
+    rotate = rotate + "deg";
+  }
   // Slider Background Color Variables
   var mainSliderColor = this.config.mainSliderColor ? this.config.mainSliderColor : "#636363";
   var secondarySliderColor = this.config.secondarySliderColor ? this.config.secondarySliderColor : "#4d4d4d";
+  var border = this.config.border ? this.config.border : "0";
   // Slider Thumb Variables
   var thumbWidth = this.config.thumbWidth ? this.config.thumbWidth : "25px";
   var thumbHeight = this.config.thumbHeight ? this.config.thumbHeight : "80px";
   var thumbColor = this.config.thumbColor ? this.config.thumbColor : "#969696";
   var thumbHorizontalPadding = this.config.thumbHorizontalPadding ? this.config.thumbHorizontalPadding : "10px";
   var thumbVerticalPadding = this.config.thumbVerticalPadding ? this.config.thumbVerticalPadding : "20px";
-  // var horizontal = this.config.horizontal ? this.config.horizontal : true;
-  // var vertical = this.config.vertical ? this.config.vertical : false;
-
-  // if (vertical == true || horizontal == false) {
-  //   horizontal = false;
-  //   vertical = true;
-  // }
-  // // else if (horizontal == false) {
-  // //   vertical = true;
-  // // }
-  // // console.log(horizontal);
-  // // console.log(vertical);
+  var thumbTop = this.config.thumpTop ? this.config.thumpTop : "calc((var(--slider-width) - var(--thumb-height)) / 2)";
+  var thumbBorderRight = this.config.thumbBorderRight ? this.config.thumbBorderRight : "var(--thumb-horizontal-padding) solid var(--slider-main-color)";
+  var thumbBorderLeft = this.config.thumbBorderLeft ? this.config.thumbBorderLeft : "var(--thumb-horizontal-padding) solid var(--slider-main-color)";
+  var thumbBorderTop = this.config.thumbBorderTop ? this.config.thumbBorderTop : "var(--thumb-vertical-padding) solid var(--slider-main-color)";
+  var thumbBorderBotton = this.config.thumbBorderBotton ? this.config.thumbBorderBotton : "var(--thumb-vertical-padding) solid var(--slider-main-color)";
+  // top: calc((var(--slider-width) - var(--thumb-height)) / 2);
+  // border-right: var(--thumb-horizontal-padding) solid var(--slider-main-color);
+  // border-left: var(--thumb-horizontal-padding) solid var(--slider-main-color);
+  // border-top: var(--thumb-vertical-padding) solid var(--slider-main-color);
+  // border-bottom: var(--thumb-vertical-padding) solid var(--slider-main-color);
 
 
   var entity = this.config.entity;
@@ -53,6 +61,7 @@ render() {
   var isInputNumber = false;
   var isMediaPlayer= false;
   var isCover = false;
+  var isFan = false;
 
   // Check if entity is light or input_number
   if (this.config.entity.includes("light.")) {
@@ -71,6 +80,10 @@ render() {
     isCover = true;
     var step = this.config.step ? this.config.step: "1";
   }
+  else if (this.config.entity.includes("fan.")) {
+    isFan = true;
+    var step = this.config.step ? this.config.step: "1";
+  }
   
   var styleStr = `
     --slider-width: ${width};
@@ -79,19 +92,31 @@ render() {
     --slider-main-color: ${mainSliderColor};
     --slider-secondary-color: ${secondarySliderColor};
     --slider-radius: ${radius};
+    --border: ${border};
     
     --thumb-width: ${thumbWidth};
     --thumb-height: ${thumbHeight};
     --thumb-color: ${thumbColor};
     --thumb-horizontal-padding: ${thumbHorizontalPadding};
     --thumb-vertical-padding: ${thumbVerticalPadding};
-  `;
 
+    --rotate: ${rotate};
+    --top: ${top};
+    --bottom: ${bottom};
+    --right: ${right};
+    --left: ${left};
+    --container-height: ${containerHeight};
+    --thumb-top: ${thumbTop};
+    --thumb-border-right: ${thumbBorderRight};
+    --thumb-border-left: ${thumbBorderLeft};
+    --thumb-border-top: ${thumbBorderTop};
+    --thumb-border-bottom: ${thumbBorderBotton};
+  `;
   if (isLight) {
     if (this.config.function == "Warmth") {
       return html`
           <ha-card>
-            <div class="slider-container" style="--slider-height: ${height};">
+            <div class="slider-container" style="${styleStr}">
               <input name="foo" type="range" class="${entityClass.state}" style="${styleStr}" .value="${entityClass.state === "off" ? 0 : entityClass.attributes.color_temp}" min="${entityClass.attributes.min_mireds}" max="${entityClass.attributes.max_mireds}" step="${step}" @change=${e => this._setWarmth(entityClass, e.target.value)}>
             </div>
           </ha-card>
@@ -100,7 +125,7 @@ render() {
     else {
       return html`
           <ha-card>
-            <div class="slider-container" style="--slider-height: ${height};">
+            <div class="slider-container" style="${styleStr}">
               <input name="foo" type="range" class="${entityClass.state}" style="${styleStr}" .value="${entityClass.state === "off" ? 0 : Math.round(entityClass.attributes.brightness/2.55)}" step="${step}" @change=${e => this._setBrightness(entityClass, e.target.value)}>
             </div>
           </ha-card>
@@ -111,7 +136,7 @@ render() {
   if (isInputNumber) {
     return html`
         <ha-card>
-          <div class="slider-container" style="--slider-height: ${height};">
+          <div class="slider-container" style="${styleStr}">
             <input name="foo" type="range" class="${entityClass.state}" style="${styleStr}" .value="${entityClass.state}" min="${entityClass.attributes.min}" max="${entityClass.attributes.max}" step="${step}" @change=${e => this._setInputNumber(entityClass, e.target.value)}>
           </div>
         </ha-card>
@@ -119,12 +144,17 @@ render() {
   }
 
   if (isMediaPlayer) {
-    var numStr = entityClass.attributes.volume_level.toFixed(2);
+    if (entityClass.attributes.volume_level != undefined) {
+      var numStr = entityClass.attributes.volume_level.toFixed(2);
+    }
+    else {
+      var numStr = "0";
+    }
     var num = Number(numStr.replace("0.", ""));
 
     return html`
         <ha-card>
-          <div class="slider-container" style="--slider-height: ${height};">
+          <div class="slider-container" style="${styleStr}">
             <input name="foo" type="range" class="${entityClass.state}" style="${styleStr}" .value="${num}" min="0" max="100" step="${step}" @change=${e => this._setMediaVolume(entityClass, e.target.value)}>
           </div>
         </ha-card>
@@ -134,7 +164,17 @@ render() {
   if (isCover) {
     return html`
         <ha-card>
-          <div class="slider-container" style="--slider-height: ${height};">
+          <div class="slider-container" style="${styleStr}">
+            <input name="foo" type="range" class="${entityClass.state}" style="${styleStr}" .value="${entityClass.attributes.current_position}" min="0" max="100" step="${step}" @change=${e => this._setCover(entityClass, e.target.value)}>
+          </div>
+        </ha-card>
+    `;
+  }
+
+  if (isFan) {
+    return html`
+        <ha-card>
+          <div class="slider-container" style="${styleStr}">
             <input name="foo" type="range" class="${entityClass.state}" style="${styleStr}" .value="${entityClass.attributes.current_position}" min="0" max="100" step="${step}" @change=${e => this._setCover(entityClass, e.target.value)}>
           </div>
         </ha-card>
@@ -145,9 +185,16 @@ render() {
 updated() {}
 
 
+_setFan(entityClass, value) {
+  this.hass.callService("fan", "set_percentage", {
+      entity_id: entityClass.entity_id,
+      percentage: value
+  });
+}
+
 _setCover(entityClass, value) {
-  console.log("Setting Cover to: " + value);
-  console.log("From Position: " + entityClass.attributes.current_position);
+  // console.log("Setting Cover to: " + value);
+  // console.log("From Position: " + entityClass.attributes.current_position);
   this.hass.callService("cover", "set_cover_position", {
       entity_id: entityClass.entity_id,
       position: value
@@ -207,8 +254,8 @@ setConfig(config) {
     throw new Error("You need to define entity");
   }
 
-  if (!config.entity.includes("input_number.") && !config.entity.includes("light.") && !config.entity.includes("media_player.") && !config.entity.includes("cover.")) {
-    throw new Error("Entity has to be a light, input_number, media_player or a cover.");
+  if (!config.entity.includes("input_number.") && !config.entity.includes("light.") && !config.entity.includes("media_player.") && !config.entity.includes("cover.") && !config.entity.includes("fan.") ) {
+    throw new Error("Entity has to be a light, input_number, media_player, cover or a fan.");
   }
   
   this.config = config;
@@ -218,34 +265,45 @@ getCardSize() {
   return 0;
 }
 
+          // -webkit-transform: rotate(270deg);
+          // -moz-transform: rotate(270deg);
+          // -o-transform: rotate(270deg);
+          // -ms-transform: rotate(270deg);
+          // transform: rotate(270deg);
 static get styles() {
   return css`
       .slider-container {
-          height: var(--slider-height);
+          height: var(--container-height);
           position: relative;
       }
 
       .slider-container input[type="range"] {
           outline: 0;
-          border: 0;
-          border-radius: var(--slider-radius);
+          border: var(--border);
           width: var(--slider-width);
+          height: var(--slider-height);
+          border-radius: var(--slider-radius);
+          background-color: var(--slider-secondary-color); /*Remaining Slider color*/
           margin: 0;
           transition: box-shadow 0.2s ease-in-out;
           overflow: hidden;
-          height: var(--slider-height);
           -webkit-appearance: none;
-          background-color: var(--slider-secondary-color); /*Remaining Slider color*/
           position: absolute;
-          top: calc(50% - (var(--slider-height) / 2));
-          right: calc(50% - (var(--slider-width) / 2));
+          top: var(--top);
+          bottom: var(--bottom);
+          right: var(--right);
+          left: var(--left);
+          -webkit-transform: rotate(var(--rotate));
+          -moz-transform: rotate(var(--rotate));
+          -o-transform: rotate(var(--rotate));
+          -ms-transform: rotate(var(--rotate));
+          transform: rotate(var(--rotate));
       }
 
       .slider-container input[type="range"]::-webkit-slider-runnable-track {
           height: var(--slider-height);
           -webkit-appearance: none;
           color: var(--slider-main-color);
-          margin-top: -1px;
           transition: box-shadow 0.2s ease-in-out;
       }
 
@@ -260,11 +318,11 @@ static get styles() {
 
           box-shadow: -3500px 0 0 3500px var(--slider-main-color), inset 0 0 0 25px var(--thumb-color);
 
-          top: calc((var(--slider-width) - var(--thumb-height)) / 2);
-          border-right: var(--thumb-horizontal-padding) solid var(--slider-main-color);
-          border-left: var(--thumb-horizontal-padding) solid var(--slider-main-color);
-          border-top: var(--thumb-vertical-padding) solid var(--slider-main-color);
-          border-bottom: var(--thumb-vertical-padding) solid var(--slider-main-color);
+          top: var(--thumb-top);
+          border-right: var(--thumb-border-right);
+          border-left: var(--thumb-border-left);
+          border-top: var(--thumb-border-top);
+          border-bottom: var(--thumb-border-bottom);
       }
 
       .slider-container input[type="range"]::-webkit-slider-thumb:hover {
